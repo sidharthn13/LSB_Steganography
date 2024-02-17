d60cf506859dc45f5ccbe91294036c8882139eb1
sidharthn13 <sidharth4network@gmail.com>
2024-02-17 21:06:33 +0530

    program modified to read all color values

--- processingImage.c
+++ processingImage.c
@@ -1,10 +1,22 @@
 #include "typeDefinitions.h"
+void displayImageData(Image *image){
+    printf("the type of file is : %s\n",image->fileType);
+    printf("the pixel array offset is : %d\n",image->pixelArrayOffset);
+    printf("the width is : %d pixels\n",image->width);
+    printf("number of bits per pixel is : %hu \n",image->bitsPerPixel);
+    printf("size of raw pixel bitmap is : %d bytes\n",image->size);
+}
 void main(){
     Image image;
     collectHeaderInfo(&image);
-    printf("the type of file is : %s\n",image.fileType);
-    printf("the pixel array offset is : %d\n",image.pixelArrayOffset);
-    printf("the width is : %d pixels\n",image.width);
-    printf("number of bits per pixel is : %hu \n",image.bitsPerPixel);
-    printf("size of raw pixel bitmap is : %d bytes\n",image.size);
+    displayImageData(&image);
+    fseek(image.fileStream, 54, SEEK_SET);  //moving the filestream pointer to the location where pixel data is stored
+    uchar pixelBuffer;
+    int count = 0;
+    while(ftell(image.fileStream)<2359350){
+        count++;
+        fread(&pixelBuffer, sizeof(uchar), 1, image.fileStream);
+        printf("%d [%d]-> ",pixelBuffer,count);
+    }
+    fclose(image.fileStream);
 }

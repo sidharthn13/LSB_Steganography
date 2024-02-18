90113223d12d3fbb85ed78baa351fe0fd504b6d7
sidharthn13 <sidharth4network@gmail.com>
2024-02-18 16:59:12 +0530

    modified the while loop in encode.c file to correct the return value of the encode function (the decryptCount variable)

--- decode.c
+++ decode.c
@@ -3,5 +3,7 @@ uint decode(Image *image){
     uint decodeResult;
     fseek(image->fileStream, 54, SEEK_SET);
     fread(&decodeResult, sizeof(uint), 1, image->fileStream);
+    
+    
     return decodeResult;
 }
--- encode.c
+++ encode.c
@@ -3,9 +3,9 @@ uint encode(Secret *secret, Image *image){
     uint decryptCount = 0;  //indicates the number of bytes in which data has been embedded in the .bmp file
     fseek(image->fileStream, 58, SEEK_SET); //the first 4 bytes of the pixel data is reserved for decrypt counter
     fseek(secret->fileStream, 0, SEEK_SET);
-    while(!feof(secret->fileStream)){
-        uchar charFromSecret;
-        uchar pixelValue;
+    uchar charFromSecret;  //buffer to store data read from secret file
+    uchar pixelValue;      //buffer to read and write data from image file
+    while(ftell(secret->fileStream) < secret->size){
         fread(&charFromSecret, sizeof(uchar), 1, secret->fileStream);
         for(int i = 7; i >= 0; i--){
             int bit = ( charFromSecret >> i ) & 1 ;
